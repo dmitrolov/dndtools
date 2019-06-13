670ca6777c2bd061384b58389dbcda380bbce638
Dmitriy Frolov <dmitrolov@gmail.com>
2019-06-13 13:36:52 +0300

    test

--- app/js/common.js
+++ app/js/common.js
@@ -8,16 +8,17 @@ var app = new Vue({
   data: {
     message: 'Привет, Vue!'
   }
-})
+});
 var app2 = new Vue({
   el: '#app-2',
   data: {
     message: 'Вы загрузили эту страницу: ' + new Date().toLocaleString()
   }
-})
+});
 var app3 = new Vue({
   el: '#app-3',
   data: {
     seen: true
   }
-})
+});
+//git test
